75230dd0140a0646d30340668d5fa6c4787495e9
cokingtins1 <106932061+cokingtins1@users.noreply.github.com>
2024-03-20 15:33:05 -0400

    fixed layout issues

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -29,7 +29,7 @@ export default function Header({ session }) {
 
 	return (
 		<>
-			<header className="bg-bg-elevated rounded-lg p-4">
+			<header className=" bg-bg-elevated rounded-lg p-4">
 				<nav className="flex items-center justify-between w-[92%] mx-auto md:grid grid-cols-3 justify-items-stretch">
 					<div className="relative z-50">
 						<Image
@@ -47,16 +47,18 @@ export default function Header({ session }) {
 
 					<div
 						className={`nav-links duration-500 md:static absolute bg-bg-elevated md:min-h-fit rounded-lg ${
-							menuOpen
-								? "top-[7%] z-40 left-4 right-4 pb-4 rounded-t-none h-[220px]"
-								: "hidden -top-full"
-						} md:w-auto flex items-center px-5 md:justify-self-end`}
+							menuOpen &&
+							size.width < 768 &&
+							"top-[7%] z-40 left-4 right-4 pb-4 rounded-t-none h-[220px]"
+						} ${
+							!menuOpen && size.width < 768 && "hidden -top-full"
+						}  md:w-auto flex items-center px-5 md:justify-self-end`}
 					>
 						<ul className="flex flex-col md:flex-row md:items-center md:gap-[4vw] gap-4">
 							{size.width < 768 && (
 								<NavBar setMenuOpen={setMenuOpen} />
 							)}
-							<div className="flex flex-col items gap-4 h-[88px]">
+							<div className="flex flex-col items gap-4 h-[88px] md:flex-row md:h-fit">
 								<AccountButton session={session} />
 								<DynamicShoppingCart />
 							</div>
