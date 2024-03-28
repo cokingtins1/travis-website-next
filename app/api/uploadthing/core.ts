57cd81ef0bce05c66c8b968ca3a2db15a3cff105
cokingtins1 <106932061+cokingtins1@users.noreply.github.com>
2024-03-28 06:55:43 -0400

    uploadThing test

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,40 @@
+import { useSession } from "@/libs/supabase/useSession";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+
+const f = createUploadthing();
+
+const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
+
+// FileRouter for your app, can contain multiple FileRoutes
+export const ourFileRouter = {
+	// Define as many FileRoutes as you like, each with a unique routeSlug
+	// imageUploader: f({ image: { maxFileSize: "4MB" } })
+	mp3Uploader: f({ "audio/mpeg": { maxFileSize: "128MB", contentDisposition: "attachment" } })
+		// wavUploader : f({"audio/x-wav": {maxFileSize: '128MB'}}),
+		// StemUploader : f({"application/zip": {maxFileSize: '1GB'}}),
+
+		// Set permissions and file types for this FileRoute
+		.middleware(async ({ req }) => {
+			// This code runs on your server before upload
+			// const user = await auth(req);
+			const { id } = await useSession();
+
+			// If you throw, the user will not be able to upload
+			if (!id) throw new UploadThingError("Unauthorized");
+
+			// Whatever is returned here is accessible in onUploadComplete as `metadata`
+			return { userId: id };
+		})
+		.onUploadComplete(async ({ metadata, file }) => {
+			// This code RUNS ON YOUR SERVER after upload
+			console.log("Upload complete for userId:", metadata.userId);
+
+			console.log("file url", file.url);
+
+			// !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+			return { uploadedBy: metadata.userId };
+		}),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
