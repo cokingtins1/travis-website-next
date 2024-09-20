a6adef9ae9bd6701eefb2a81a0579cb937d118fe
Sean Cokingtin <cokingtins1@gmail.com>
2024-09-20 18:23:14 -0400

    updated purchasing terms

--- app/components/Store Components/Pricing Component/UsageTerms.jsx
+++ app/components/Store Components/Pricing Component/UsageTerms.jsx
@@ -4,56 +4,73 @@ import CellTowerIcon from "@mui/icons-material/CellTower";
 import VideocamIcon from "@mui/icons-material/Videocam";
 import ReceiptLongIcon from "@mui/icons-material/ReceiptLong";
 import RemoveIcon from "@mui/icons-material/Remove";
+import GroupsIcon from "@mui/icons-material/Groups";
+import RadioIcon from "@mui/icons-material/Radio";
 
 export default function UsageTerms({ selected }) {
-	const icons = [
-		{ key: 0, value: <MicNoneIcon /> },
-		{ key: 1, value: <FilterNoneIcon /> },
-		{ key: 2, value: <CellTowerIcon /> },
-		{ key: 3, value: <VideocamIcon /> },
-		{ key: 4, value: <ReceiptLongIcon /> },
-		{ key: 5, value: <RemoveIcon /> },
-	];
+  //selected = "default" || "basic" || "premium" || "exclusive"
 
-	const terms = {
-		default: ["select a license to view terms"],
-		basic: [
-			"used for recording",
-			"distribute up to 1,000 copies",
-			"25,000 online audio streams",
-			"1 music video",
-		],
+  const commonIcons = [
+    <MicNoneIcon />,
+    <FilterNoneIcon />,
+    <CellTowerIcon />,
+    <VideocamIcon />,
+    <ReceiptLongIcon />,
+    <GroupsIcon />,
+    <RadioIcon />,
+  ];
 
-		premium: [
-			"used for recording",
-			"distribute up to 10,000 copies",
-			"300,000 online audio streams",
-			"1 music video",
-		],
+  const terms = {
+    default: ["select a license to view terms"],
+    basic: {
+      text: [
+        "used for recording",
+        "distribute up to 3,000 copies",
+        "500,000 online audio streams",
+        "1 music video",
+        "for profit live performances",
+        "radio broadcasting rights (2 stations)",
+      ],
+      icon: commonIcons,
+    },
 
-		exclusive: [
-			"used for recording",
-			"distribute unlimited copies",
-			"unlimited online audio streams",
-			"unlimited music videos",
-			"radio broadcasting rights",
-			"beat removed from store",
-		],
-	};
+    premium: {
+      text: [
+        "used for recording",
+        "distribute up to 10,000 copies",
+        "500,000 online audio streams",
+        "1 music video",
+        "for profit live performances",
+        "radio broadcasting rights (2 stations)",
+      ],
+      icon: commonIcons,
+    },
 
-	const selectedTerms = terms[selected] || null;
+    exclusive: {
+      text: [
+        "used for recording",
+        "distribute unlimited copies",
+        "unlimited online audio streams",
+        "unlimited music videos",
+        "for profit live performances",
+        "radio broadcasting rights (2 stations)",
+      ],
+      icon: commonIcons,
+    },
+  };
 
-	return (
-		<ul className="flex flex-col md:grid grid-cols-3 gap-4">
-			{selected &&
-				selectedTerms &&
-				selectedTerms.map((t, index) => (
-					<li key={index} className="text-text-secondary text-sm">
-						<span className="flex items-center gap-2">
-							{selected !== "default" && icons[index].value} {t.toUpperCase()}
-						</span>
-					</li>
-				))}
-		</ul>
-	);
+  const selectedTerms = terms[selected] || null;
+
+  return (
+    <ul className="flex flex-col md:grid grid-cols-3 gap-4">
+      {selected !== "default" &&
+        selectedTerms?.text.map((t, index) => (
+          <li key={index} className="text-text-secondary text-sm">
+            <span className="flex items-center gap-2">
+              {selectedTerms.icon[index]} {t.toUpperCase()}
+            </span>
+          </li>
+        ))}
+    </ul>
+  );
 }
